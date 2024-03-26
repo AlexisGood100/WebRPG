8f2b0c4ca36b3d649796cc0e242776b0863ef7ef
Alexander Wolfe <LogicCreates123@Gmail.com>
2024-03-25 23:37:36 -0400

    further reduced redunancy in function for creating random familiars

--- FamiliarFunctions/createRandomFamiliar.js
+++ FamiliarFunctions/createRandomFamiliar.js
@@ -4,9 +4,9 @@ const randomNumber = (bound) =>{
 }
 
 
-function createRandomFamiliar(imgArray, difficulty){
+const createRandomFamiliar = (imgArray, difficulty)=>{
     let nameArray = ['Godble', 'Jackson', 'JapBat', 'Squiggles', 'Razha', 'Yabado', 'Bobo']
-    let nameCalc = Math.floor(Math.random()*3) + 1
+    let nameCalc = Math.floor(Math.random()*nameArray.length - 1) + 1
     let specialAttackArrayIndex = Math.floor(Math.random() * 4) + 1;
     let specialAttackArray1 = ['Slash', 'Sandstrike', 'Magic Bolt', 'Dragon Breath', 'Firestorm']
     let specialAttackArray2 = ['Slash', 'Sandstrike', 'Magic Bolt', 'Dragon Breath', 'Firestorm']
@@ -14,18 +14,18 @@ function createRandomFamiliar(imgArray, difficulty){
     let specialAbility1Des = specialAttackArray1[specialAttackArrayIndex]
     let specialAbility2Des = specialAttackArray2[specialAttackArrayIndex]
     let specialAbility3Des = specialAttackArray3[specialAttackArrayIndex]
-    let constitution = parseFloat(Math.floor(Math.random() * (randomNumber(20) +10)* difficulty));
-    let dexterity = (Math.floor(Math.random() * randomNumber(20)+ 10) * difficulty);
-    let strength = (Math.floor(Math.random() * randomNumber(20)+ 10) * difficulty);
-    let charisma = (Math.floor(Math.random() * randomNumber(20)+ 10));
-    let willpower = (Math.floor(Math.random() * randomNumber(20)+ 10));
-    let intelligence = (Math.floor(Math.random() * randomNumber(20)+ 10));
-    let defense = (Math.floor(Math.random() * randomNumber(20)+ 10));
-    let speed = 0;
+    let startingStat = (Math.floor(Math.random() * randomNumber(20)+ 10) * difficulty)
+    let constitution = startingStat;
+    let dexterity = startingStat;
+    let strength = startingStat;
+    let charisma = startingStat;
+    let willpower = startingStat;
+    let intelligence = startingStat;
+    let defense = startingStat;
+    let speed =  startingStat;
     let cost = (Math.floor(Math.random() * randomNumber(4)) + 1);
      let typeArray = ['Water', 'Fire', 'Earth', 'Air', 'Light', 'Dark'];
      let typeArrayRandom = typeArray[Math.round(Math.random() * 5)]
-     
     let imgIndex = Math.floor(Math.random() * 20) + 1;
     let randomFamiliar = new Familiar(nameArray[nameCalc], typeArrayRandom, imgArray[imgIndex], constitution, strength, intelligence, dexterity, charisma, willpower,defense, speed, null, null, null, specialAbility1Des, specialAbility2Des, specialAbility3Des, cost)
     randomFamiliar.generateAttackPower()
